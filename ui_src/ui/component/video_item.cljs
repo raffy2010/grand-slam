17b16a25fee55fcc21958557141ee8f858d864d4
Raffy Li <raffy2010@gmail.com>
2017-04-06 18:43:28 +0800

    include Reanimated to handle animation

--- ui_src/ui/component/video_item.cljs
+++ ui_src/ui/component/video_item.cljs
@@ -2,6 +2,7 @@
  (:require [reagent.core :as r]
            [cljs-react-material-ui.reagent :as ui]
            [cljs-react-material-ui.icons :as ic]
+           [reanimated.core :as anim]
            [ui.state :refer [active-files tasks]]
            [ui.ffmpeg :refer [cancel-convert]]
            [ui.component.video-thumbnail :refer [video-thumbnail]]
@@ -37,12 +38,55 @@
 
 (defn find-task
   [file]
-  (let [file-id (get file "id")]
+  (let [file-id (:id file)]
     (->> @tasks
          vals
          (filter #(= file-id
                      (:file-id %)))
          first)))
+
+(defn cancel-btn
+  [task]
+  [:div {:style {:top 0
+                 :right 0}}
+   [ui/icon-button
+    {:on-click (partial handle-cancel task)}
+    (ic/content-clear)]])
+
+(defn action-btns
+  [file]
+  [:div {:style {:top 0
+                 :right 0
+                 :width 144}}
+   [ui/icon-button {:class (if (:detailed file) "fold" "more")
+                    :on-click (partial toggle-file-detail file)}
+    (if (:detailed file)
+      (ic/navigation-expand-less)
+      (ic/navigation-expand-more))]
+   [ui/icon-menu {:use-layer-for-click-away true
+                  :icon-button-element (r/as-element
+                                         [ui/icon-button {:on-click #(.stopPropagation %)}
+                                          (ic/action-swap-horiz)])
+                  :anchor-origin {:horizontal "right" :vertical "top"}
+                  :target-origin {:horizontal "right" :vertical "top"}
+                  :on-item-touch-tap (partial toggle-convert-modal file)}
+    [ui/menu-item {:primary-text "type"
+                   :type "video-type"}]
+    [ui/menu-item {:primary-text "quality"
+                   :type "video-quality"}]
+    [ui/menu-item {:primary-text "dimension"
+                   :type "video-dimension"}]
+    [ui/menu-item {:primary-text "preset"
+                   :type "video-preset"}]
+    [ui/menu-item {:primary-text "custom"
+                   :type "advance"}]]
+   [ui/icon-menu {:use-layer-for-click-away true
+                  :icon-button-element (r/as-element
+                                         [ui/icon-button {:on-click #(.stopPropagation %)}
+                                          (ic/navigation-more-vert)])
+                  :anchor-origin {:horizontal "right" :vertical "top"}
+                  :target-origin {:horizontal "right" :vertical "top"}}
+    [ui/menu-item {:primary-text "type"}]]])
 
 (defn video-item
   "video item card"
@@ -55,50 +99,29 @@
             :on-click (partial toggle-file-select file)}
       [video-thumbnail file]
       [:div {:class "video-info"}
-       (for [field ["filename" "bit_rate" "size" "duration"]]
+       (for [field [:filename :bit_rate :size :duration]]
          ^{:key field}
-         [:p (str field ": " (get-in file ["format" field]))])]
+         [:p (str field ": " (get-in file [:format field]))])]
       (if-not (nil? task) [ui/linear-progress {:class "convert-progress-bar"
                                                :mode "determinate"
                                                :value (get-in task [:process :progress])}])
       [:div {:class "action-menu"}
-       (if-not (nil? task)
-         [ui/icon-button {:on-click (partial handle-cancel task)}
-          (ic/content-clear)]
-         [:div
-          [ui/icon-button {:class (if (:detailed file) "fold" "more")
-                           :on-click (partial toggle-file-detail file)}
-           (if (:detailed file)
-             (ic/navigation-expand-less)
-             (ic/navigation-expand-more))]
-          [ui/icon-menu {:use-layer-for-click-away true
-                         :icon-button-element (r/as-element
-                                                [ui/icon-button {:on-click #(.stopPropagation %)}
-                                                 (ic/action-swap-horiz)])
-                         :anchor-origin {:horizontal "right" :vertical "top"}
-                         :target-origin {:horizontal "right" :vertical "top"}
-                         :on-item-touch-tap (partial toggle-convert-modal file)}
-           [ui/menu-item {:primary-text "type"
-                          :type "video-type"}]
-           [ui/menu-item {:primary-text "quality"
-                          :type "video-quality"}]
-           [ui/menu-item {:primary-text "dimension"
-                          :type "video-dimension"}]
-           [ui/menu-item {:primary-text "preset"
-                          :type "video-preset"}]
-           [ui/menu-item {:primary-text "custom"
-                          :type "advance"}]]
-          [ui/icon-menu {:use-layer-for-click-away true
-                         :icon-button-element (r/as-element
-                                                [ui/icon-button {:on-click #(.stopPropagation %)}
-                                                 (ic/navigation-more-vert)])
-                         :anchor-origin {:horizontal "right" :vertical "top"}
-                         :target-origin {:horizontal "right" :vertical "top"}}
-           [ui/menu-item {:primary-text "type"}]]])]
+         [anim/css-transition-group
+          {:transition-name "cancel-btn"
+           :transition-enter-timeout 500
+           :transition-leave-timeout 500}
+          (if-not (nil? task)
+            (cancel-btn task))]
+         [anim/css-transition-group
+          {:transition-name "action-btn"
+           :transition-enter-timeout 500
+           :transition-leave-timeout 500}
+          (if (nil? task)
+            (action-btns file))]]
       [:div {:class (str "video-stream-detail"
                          (if (:more-detail file) " detail-active"))
              :on-click #(.stopPropagation %)}
-       (for [stream (get file "streams")]
+       (for [stream (:streams file)]
          ^{:key stream}
          [stream-item stream])]
       (when-not (nil? (:convert-mode file))
